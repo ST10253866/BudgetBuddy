58d13fad6728f743b517a6c698dca2807a1b0ef2
ST10258124 <vivekrajaram888@gmail.com>
2025-04-26 16:52:23 +0200

    Added Intents for login and reg
    made progress on naviagtion.
    
    TODO:
    -add back buttons on some views
    -make nav bar work

--- app/src/main/java/vcmsa/projects/budgetbuddy/Login.kt
+++ app/src/main/java/vcmsa/projects/budgetbuddy/Login.kt
@@ -1,4 +1,31 @@
 package vcmsa.projects.budgetbuddy
 
-class Login {
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import vcmsa.projects.budgetbuddy.databinding.FragmentLoginBinding
+
+class Login : AppCompatActivity() {
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = FragmentLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        enableEdgeToEdge()
+        setContentView(R.layout.fragment_login)
+
+        binding.tvGoToRegister.setOnClickListener {
+            val i = Intent(this, Register::class.java).apply {  }
+            startActivity(i)
+        }
+
+        binding.btnLogin.setOnClickListener {
+            val i = Intent(this, Hom::class.java).apply {  }
+            startActivity(i)
+        }
+    }
 }
--- app/src/main/java/vcmsa/projects/budgetbuddy/Register.kt
+++ app/src/main/java/vcmsa/projects/budgetbuddy/Register.kt
@@ -1,4 +1,27 @@
 package vcmsa.projects.budgetbuddy
 
-class Register {
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import vcmsa.projects.budgetbuddy.databinding.FragmentRegisterBinding
+
+class Register : AppCompatActivity() {
+    private lateinit var binding: FragmentRegisterBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = FragmentRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnRegister.setOnClickListener {
+            val i = Intent(this, Login::class.java).apply {  }
+            startActivity(i)//maybe dont make this redirect idk...maybe just make it make acc
+        }
+
+        binding.tvGoToLogin.setOnClickListener {
+            val i = Intent(this, Login::class.java).apply {  }
+            startActivity(i)
+        }
+    }
 }
